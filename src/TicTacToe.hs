9a1a4ba8282850320cf9acff2741c20caa6a97ce
Rushi Shah <2016rshah@gmail.com>
2015-09-19 10:12:32 -0400

    Finished AI??

--- src/TicTacToe.hs
+++ src/TicTacToe.hs
@@ -1,16 +1,31 @@
+{-# LANGUAGE FlexibleInstances #-}
 module TicTacToe where
 
 import Data.Either
+import Safe
+import Data.Maybe
+import Data.List
+import Data.List.Split
 
 data Symbol = X | O 
 	deriving (Show, Eq)
 
 type Piece = Either Int Symbol
 
+showHuman :: Piece -> Char
+showHuman (Left x) = '.'
+showHuman (Right x) = head (show x)
+
 type Three = [Piece] -- how do I constrain this to a length of three?
 
 data Board = Board Three Three Three
-	deriving (Show, Eq)
+	deriving (Eq)
+
+instance Show Board where
+	show board@(Board x@[a, b, c] y@[d, e, f] z@[g, h, i]) 
+		= intercalate "\n" (chunksOf 3 (map showHuman (boardToList board)))
+
+--show (Board x@[a, b, c] y@[d, e, f] z@[g, h, i]) = intercalate "\n" (map show (x:y:z:[]))
 
 emptyBoard :: Board
 emptyBoard = Board [Left 1, Left 2, Left 3] [Left 4, Left 5, Left 6] [Left 7, Left 8, Left 9]
@@ -32,5 +47,42 @@ cols (Board x@[a, b, c] y@[d, e, f] z@[g, h, i]) = [[a, d, g], [b, e, h], [c, f,
 
 diags :: Board -> [Three]
 diags (Board x@[a, b, c] y@[d, e, f] z@[g, h, i]) = [[a, e, i], [c, e, g]]
+
+possibleMoves :: Board -> [Piece]
+possibleMoves board = filter isLeft (boardToList board)
+
+boardToList :: Board -> [Piece]
+boardToList (Board x y z) = x ++ y ++ z
+
+listToBoard :: [Piece] -> Board
+listToBoard [a,b,c,d,e,f,g,h,i] = Board [a,b,c] [d,e,f] [g,h,i]
+
+findAndReplace :: Board -> Piece -> Piece -> Board
+findAndReplace b p1 p2 = listToBoard [if x==p1 then p2 else x | x <- bl]
+	where bl = boardToList b
+
+winningXMove :: Board -> Maybe Board
+winningXMove b = headMay [findAndReplace b p (Right X) | p <- (possibleMoves b), won (findAndReplace b p (Right X))]
+
+blockOWin :: Board -> Maybe Board
+blockOWin b = headMay [findAndReplace b p (Right X) | p <- (possibleMoves b), won (findAndReplace b p (Right O))]
+
+isFork :: Board -> Bool
+isFork b = 2 == length [findAndReplace b p (Right X) | p <- (possibleMoves b), won (findAndReplace b p (Right X))]
+
+forkX :: Board -> Maybe Board
+forkX b = headMay [findAndReplace b p (Right X) | p <- (possibleMoves b), isFork (findAndReplace b p (Right X))]
+
+blockOFork :: Board -> Maybe Board
+blockOFork b = headMay [findAndReplace b p (Right X) | p <- (possibleMoves b), isFork (findAndReplace b p (Right O))]
+
+makeXMove :: Board -> Board
+makeXMove board@(Board x@[a, b, c] y@[d, e, f] z@[g, h, i])
+	| isJust (winningXMove board) = fromJust (winningXMove board)
+	| isJust (blockOWin board) = fromJust (blockOWin board)
+	| isJust (forkX board) = fromJust (forkX board)
+	| isJust (blockOFork board) = fromJust (blockOFork board)
+	| e `elem` (possibleMoves board) = findAndReplace board e (Right X)
+	| otherwise = findAndReplace board (head (possibleMoves board)) (Right X)
 
 
